7714b50a75d034df8145acf0c74dd5ead3f8d5ab
Michael Timberg <timberg.1@osu.edu>
2016-06-10 13:14:59 -0400

    RomanNumerals Passed!
    Passed third test.

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,10 +1,16 @@
 
 class RomanNumerals
   def self.arabic_to_roman (arabic)
+    roman_numerals = ['I', 'IV']
+
     roman = ''
 
-    arabic.times do
-      roman << 'I'
+    if arabic <= 3
+      arabic.times do
+        roman << roman_numerals[0]
+      end
+    else
+      roman << roman_numerals[1]
     end
 
     roman
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -10,4 +10,8 @@ describe RomanNumerals do
   it 'returns "II" when given 2' do
     expect(RomanNumerals.arabic_to_roman(2)).to eq ('II')
   end
+
+  it 'returns "IV" when given 4' do
+    expect(RomanNumerals.arabic_to_roman(4)).to eq ('IV')
+  end
 end
